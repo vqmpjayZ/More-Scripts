8b801cd2b6d74f7eee0f62b478d024f0b19b5037
vqmpjay <bakirbutkovic13@gmail.com>
2025-08-22 15:09:51 +0200

    Update Vadrifts-Horrific-Housing.lua

--- Vadrifts-Horrific-Housing.lua
+++ Vadrifts-Horrific-Housing.lua
@@ -1,11 +1,2 @@
---[[
- __   __   ______     _____     ______     __     ______   ______   ______    
-/\ \ / /  /\  __ \   /\  __-.  /\  == \   /\ \   /\  ___\ /\__  _\ /\  ___\   
-\ \ \'/   \ \  __ \  \ \ \/\ \ \ \  __<   \ \ \  \ \  __\ \/_/\ \/ \ \___  \  
- \ \__|    \ \_\ \_\  \ \____-  \ \_\ \_\  \ \_\  \ \_\      \ \_\  \/\_____\ 
-  \/_/      \/_/\/_/   \/____/   \/_/ /_/   \/_/   \/_/       \/_/   \/_____/ 
-
-dsc.gg/vadriftz (in browsers)
-]]
-
+--new loadstring
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/Vadrifts-Hub/refs/heads/main/Games/Horrific-Housing/source.lua"))()
