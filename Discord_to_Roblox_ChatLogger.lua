104d34a54a196410689a385c02a874c4100a9394
vqmpjay <bakirbutkovic13@gmail.com>
2026-01-13 19:09:57 +0100

    Fixed after a year and a half!

--- Discord_to_Roblox_ChatLogger.lua
+++ Discord_to_Roblox_ChatLogger.lua
@@ -1,7 +1,7 @@
 --[[
 re-scripted darks script cz his dumbahh got his website removed
 i also fixed the syn.request shi
-returning to this in 2026 i made it work for textchatservice since legacy is gone
+returning to this in 2026 i made it work for textchatservice since legacy chat is gone
 
 Tutorial:
 make a discord webhook
@@ -20,7 +20,7 @@ if not success then
     return
 end
 
-local wh = "https://discord.com/api/webhooks/1404578641698095234/D74JEPk8etkx0xgUVN4l-ZF2vYA32LZdw4uzK4cHwUU-zqg0ktXekiJH7jL2RcOojBCe"
+local wh = "Enter your Discord webhook link here!"
 
 local startEmbed = {
     ["title"] = "Beginning of Message logs on " .. info.Name .. " at " .. tostring(os.date("%m/%d/%y at time %X"))
